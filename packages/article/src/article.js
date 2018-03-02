6c09f3269efcdef0c6a5b91d849611b40a399a7a
tuncaulubilge <tunca.ulubilge@gmail.com>
2018-03-02 15:56:46 +0000

    Analytics stream for article

--- packages/article/src/article.js
+++ packages/article/src/article.js
@@ -1,21 +1,26 @@
 // @flow
 
-import React from "react"
-import { ArticleProvider } from "@times-components/provider"
+import React from "react";
+import { ArticleProvider } from "@times-components/provider";
 import withClient from "@thetimes/with-client";
-import Article from "@times-components/article"
+import Article from "@times-components/article";
 
-const ArticleDetailsPage = ({ articleId }) => (
+type ArticleProps = {
+  articleId: string,
+  analyticsStream: (data: any) => void
+};
+
+const ArticleDetailsPage = ({ articleId, analyticsStream }: ArticleProps) => (
   <ArticleProvider id={articleId} debounceTimeMs={100}>
     {({ article, isLoading, error }) => (
       <Article
         article={article}
         isLoading={isLoading}
         error={error}
-        analyticsStream={() => { }}
+        analyticsStream={analyticsStream}
       />
     )}
   </ArticleProvider>
-)
+);
 
-export default withClient(ArticleDetailsPage)
+export default withClient(ArticleDetailsPage);
