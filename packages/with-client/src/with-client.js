7d1edd3b8be221ae1543df50b7df7fea69da5301
tuncaulubilge <tunca.ulubilge@gmail.com>
2018-04-06 13:13:33 +0100

    Version bump for all packages and fix on with-client for new version

--- packages/with-client/src/with-client.js
+++ packages/with-client/src/with-client.js
@@ -9,7 +9,7 @@ import {
   IntrospectionFragmentMatcher
 } from "apollo-cache-inmemory";
 import { ApolloProvider } from "react-apollo";
-import { fragmentMatcher } from "@times-components/utils/graphql";
+import { fragmentMatcher } from "@times-components/utils";
 
 interface Config {
   graphqlEndPoint: string;
