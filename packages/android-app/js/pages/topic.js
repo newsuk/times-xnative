dc76e2d803b420f3c6b4b51565ca39f1ff8d5d21
tuncaulubilge <tunca.ulubilge@gmail.com>
2018-04-27 12:14:45 +0100

    Remove unused fetch

--- packages/android-app/js/pages/topic.js
+++ packages/android-app/js/pages/topic.js
@@ -6,7 +6,6 @@ import TopicPage from "@thetimes/topic";
 
 const config = NativeModules.ReactConfig;
 const { track } = NativeModules.ReactAnalytics;
-const { fetch } = NativeModules.NativeFetch;
 const { onArticlePress } = NativeModules.TopicEvents;
 
 const View = ({ topicSlug }: { topicSlug: string }) => (
