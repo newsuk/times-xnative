82d0401ac256e7c7a8a87ef719be7df4a423122d
tuncaulubilge <tunca.ulubilge@gmail.com>
2018-05-01 13:20:21 +0100

    Add ios-app package for js bundling with haul

---
+++ packages/ios-app/js/pages/article.js
@@ -0,0 +1,37 @@
+// @flow
+
+import React from "react";
+import PropTypes from "prop-types";
+import { NativeModules } from "react-native";
+import ArticlePage from "@thetimes/article";
+import { platformAdConfig } from "../ad-config/platform-ad-config";
+
+const {
+  onRelatedArticlePress,
+  onLinkPress,
+  onAuthorPress,
+  onVideoPress
+} = NativeModules.NativeModuleArticleActions;
+const config = NativeModules.NativeModuleReactConfig;
+const { fetch } = NativeModules.NativeModuleFetch;
+const { track } = NativeModules.NativeModuleAnalytics;
+
+const ArticlePageView = ArticlePage(config)(fetch);
+
+const Article = ({ articleId }: { articleId: string }) => (
+  <ArticlePageView
+    articleId={articleId}
+    analyticsStream={track}
+    onArticlePress={url => onRelatedArticlePress(url)}
+    onLinkPress={url => onLinkPress(url)}
+    onAuthorPress={slug => onAuthorPress(slug)}
+    onVideoPress={extras => onVideoPress(extras)}
+    platformAdConfig={platformAdConfig({
+      sectionId: "9e51e2a0-fadf-11e7-9a34-94e1b34681c3",
+      sectionName: "News",
+      articlePositionInSlot: 3
+    })}
+  />
+);
+
+export default Article;
