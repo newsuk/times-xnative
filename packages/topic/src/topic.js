2dd57eb0d31bbb2e098b7cc8f569533d893ed747
tuncaulubilge <tunca.ulubilge@gmail.com>
2018-05-23 11:36:43 +0100

    Lint

--- packages/topic/src/topic.js
+++ packages/topic/src/topic.js
@@ -16,21 +16,21 @@ const TopicPage = ({
   onArticlePress,
   analyticsStream
 }: TopicProps) => (
-    <TopicProvider slug={topicSlug} page={1} pageSize={20} debounceTimeMs={250}>
-      {({ topic, isLoading, error, page, pageSize, onNext, onPrev, refetch }) => (
-        <Topic
-          topic={topic}
-          slug={topicSlug}
-          isLoading={isLoading}
-          error={error}
-          onArticlePress={(event, extras) => onArticlePress(extras.url)}
-          analyticsStream={analyticsStream}
-          page={page}
-          refetch={refetch}
-          pageSize={pageSize}
-        />
-      )}
-    </TopicProvider>
-  );
+  <TopicProvider slug={topicSlug} page={1} pageSize={20} debounceTimeMs={250}>
+    {({ topic, isLoading, error, page, pageSize, onNext, onPrev, refetch }) => (
+      <Topic
+        topic={topic}
+        slug={topicSlug}
+        isLoading={isLoading}
+        error={error}
+        onArticlePress={(event, extras) => onArticlePress(extras.url)}
+        analyticsStream={analyticsStream}
+        page={page}
+        refetch={refetch}
+        pageSize={pageSize}
+      />
+    )}
+  </TopicProvider>
+);
 
 export default withClient(TopicPage);
